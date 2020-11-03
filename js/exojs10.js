b26711d02503c489825658d5274d2e90a8791103
yvandouenel <y.douenel@coopernet.fr>
2020-11-03 15:05:31 +0100

    memo (exo10) corrigé.

--- js/exojs10.js
+++ js/exojs10.js
@@ -1,7 +1,8 @@
 class Card {
-  constructor(question, answer) {
+  constructor(question, answer, explanation = "") {
     this.question = question;
     this.answer = answer;
+    this.explanation = explanation;
 
     // Dessine la carte et récupère les éléments du DOM correspondants
     this.dom_elements = this.drawCard();
@@ -25,6 +26,7 @@ class Card {
       // Récupération des valeurs des inputs pour la question et la réponse
       const question = this.dom_elements.up_question.value;
       const answer = this.dom_elements.up_answer.value;
+      const explanation = this.dom_elements.up_explanation.value;
 
       if(question) {
         this.dom_elements.question.textContent = question;
@@ -34,27 +36,39 @@ class Card {
         this.dom_elements.answer.textContent = answer;
         this.answer = answer;
       }
+      if(explanation) {
+        this.dom_elements.explanation.textContent = explanation;
+        this.explanation = explanation;
+      }
     }
   }
   drawCard = function() {
     const article = this.createCustomElement("article", document.body, "", {"class": "card"});
     const h2 = this.createCustomElement("h2", article, this.question);
     const h3 = this.createCustomElement("h3", article, this.answer);
+    const p = this.createCustomElement("p", article, this.explanation);
+
+
     const delete_button = this.createCustomElement("button", article, "Supprimer");
     const up_form = this.createCustomElement("form", article);
     this.createCustomElement("label", up_form, "Question");
     const up_question = this.createCustomElement("input", up_form);
     this.createCustomElement("label", up_form, "Réponse");
     const up_answer = this.createCustomElement("input", up_form);
+    this.createCustomElement("label", up_form, "Explication");
+    const up_explanation = this.createCustomElement("input", up_form);
     this.createCustomElement("button", up_form, "Modifier");
+    
     return {
       article: article,
       question: h2,
       answer: h3,
+      explanation: p,
       delete_button: delete_button,
       up_form: up_form,
       up_question: up_question,
-      up_answer: up_answer
+      up_answer: up_answer,
+      up_explanation: up_explanation
     };
   }
   createCustomElement = function(tagname, tagparent, tagtext = "", tagattributes = {}) {
@@ -82,8 +96,9 @@ form.onsubmit = function(event) {
 
   const question = document.querySelector("#question").value;
   const answer = document.querySelector("#answer").value;
+  const explanation = document.querySelector("#explanation").value;
   
-  const card = new Card(question, answer);
+  const card = new Card(question, answer, explanation);
 }
 
 
