e134db88ba88d9c10337f2d6f3b40234879def4d
yvandouenel <y.douenel@coopernet.fr>
2020-11-09 11:31:08 +0100

    classes Card et Column modifiées.

--- js/classes/Card.js
+++ js/classes/Card.js
@@ -1,14 +1,20 @@
 import DomElements from "./DomElements.js";
 
 export default class Card extends DomElements {
-  constructor(question, answer) {
+  constructor(question, answer, explanation, col_dom_section) {
     super();
     this.question = question;
     this.answer = answer;
+    this.explanation = explanation;
 
-    this.render();
+    this.render(col_dom_section);
   }
-  render = () => {
+  render = (col_dom_section) => {
     console.log('Dans render de Card');
+    const article =  this.createCustomElement("article", col_dom_section);
+    const h4 = this.createCustomElement("h4", article, this.question);
+    const h5 = this.createCustomElement("h5", article, this.answer);
+    const p = this.createCustomElement("p", article, this.explanation);
+
   }
 }
--- js/classes/Column.js
+++ js/classes/Column.js
@@ -1,3 +1,4 @@
+import Card from "./Card.js";
 import DomElements from "./DomElements.js";
 
 export default class Column extends DomElements{
@@ -6,19 +7,25 @@ export default class Column extends DomElements{
     this.cards = cards;
     this.title = title;
 
-    this.render();
+    // affichage de la colonne courante
+    this.dom_section = this.render();
 
     // Création des cartes
     this.createCards();
   }
   createCards = () => {
     console.log('Dans createCards');
-    
+    for(let c of this.cards) {
+      const card = new Card(c.question, c.reponse, c.explication, this.dom_section);
+    }
+
   }
   render = () => {
     console.log('Dans render de Column');
     const section =  this.createCustomElement("section", document.body);
     const title = this.createCustomElement("h3", section, this.title)
+
+    return section;
   }
   
 }
