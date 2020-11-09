8dd44d18fa8e2153ef6c24f6a261af28681acb79
yvandouenel <y.douenel@coopernet.fr>
2020-11-09 12:04:20 +0100

    Passage du fonctionnel à l'objet pour exojs15.js (refactoring)

--- js/exojs15.js
+++ js/exojs15.js
@@ -1,47 +1,60 @@
 import Coopernet from "./classes/Coopernet.js";
 import Term from "./classes/Term.js";
 import Column from "./classes/Column.js";
+import DomElements from "./classes/DomElements.js";
 
-const coop = new Coopernet("https://www.coopernet.fr");
-getTokenUserTerms();
-let token, user;
+class Table extends DomElements {
+  constructor() {
+    super();
+    this.coop = new Coopernet("https://www.coopernet.fr");
+    this.token = null;
+    this.user = null;
 
-// Récupération des cartes liées au term surlequel j'ai cliqué
-
-async function getTokenUserTerms() {
-  try {
-    // Récupération du token
-    token = await coop.getToken();
-    console.log('token : ', token);
-
-    // Récupération du n° de user dans la base de données de coopernet
-    user = await coop.getUser("y.beneito", "y.beneito", token);
-    console.log('user : ', user);
-
-    // Récupération des rubriques (terms ex : HTML) 
-    const terms = await coop.getTerms(user, token);
-    console.log('terms : ', terms);
-
-    // Affichage des termes dans une boule for of
-    for (let term of terms) {
-      const t = new Term(term.id, term.pid, term.name, handleClickTerm);
+    // Appel de getTokenUserTerms 
+    this.getTokenUserTerms();
+  }
+  getTokenUserTerms = async () => {
+    try {
+      // Récupération du this.token
+      this.token = await  this.coop.getToken();
+      console.log('token : ', this.token);
+  
+      // Récupération du n° de user dans la base de données de coopernet
+      this.user = await  this.coop.getUser("y.beneito", "y.beneito", this.token);
+      console.log('user : ', this.user);
+  
+      // Récupération des rubriques (terms ex : HTML) 
+      const terms = await  this.coop.getTerms(this.user, this.token);
+      console.log('terms : ', terms);
+  
+      // Affichage des termes dans une boule for of
+      for (let term of terms) {
+        const t = new Term(term.id, term.pid, term.name, this.handleClickTerm);
+      }
+    }
+    catch (error) {
+      console.log('Erreur : ', error);
     }
   }
-  catch (error) {
-    console.log('Erreur : ', error);
+  handleClickTerm = async (tid) => {
+    // Récupération des cartes liées au term surlequel j'ai cliqué
+    const data = await  this.coop.getCards(this.user, this.token, tid);
+    this.createColumns(data);
+  }
+  createColumns = (data) => {
+    // boucle sur le tableau des columns récupéré via Terms
+    for(let cl of data) {
+      console.log('column : ', cl);
+      // instanciation des colonnes
+      const column = new Column(cl.cartes, cl.name);
+    }
   }
 }
-async function handleClickTerm(tid) {
-  const data = await coop.getCards(user, token, tid);
-  createColumns(data);
-}
-function createColumns(data) {
-  // boucle sur le tableau des columns récupéré via Terms
-  for(let cl of data) {
-    console.log('column : ', cl);
-    // instanciation des colonnes
-    const column = new Column(cl.cartes, cl.name);
-  }
-}
+// Instanciation du tableau
+const t = new Table();
+
+
+
+
 
 
